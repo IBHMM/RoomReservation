70229715366e8a72b0676e2bbc34ada26cd5f22b
Ibrahim Muradov <136456526+IBHMM@users.noreply.github.com>
2024-08-22 16:02:57 +0400

    djskfsd

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import Header from '../components/Header';
 import FloorPlan from '../components/FloorPlan';
-import { Container } from '@mui/material';
+import { Container, Typography, Link } from '@mui/material';
 
 const MainPage: React.FC = () => {
   return (
     <>
       <Header />
       <Container>
+        <Typography variant="body1" align="center" marginTop={4}>
+          <Link href="https://github.com/IBHMM/RoomReservation" target="_blank" rel="noopener">
+            View the GitHub repository
+          </Link>
+        </Typography>
         <FloorPlan />
       </Container>
     </>
